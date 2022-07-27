19c20c28eb82fff6f10020c4690814664f38f2d4
Santiago Caci <santiago.caci@gmail.com>
2022-07-27 13:47:04 -0300

    Routing aplicado

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Home, About, Store } from './pages';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
+    <Container className='mb-5'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/about' element={<About />} />
+
+        <Route path='/*' element={<Navigate to='/' />} />
+      </Routes>
+    </Container>
   );
 }
 
